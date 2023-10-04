1e683f408f571bf4793b1c67001dc850bca679f8
Ben Cedar <ben.cedar@ravenind.com>
2023-10-03 20:06:45 -0500

    feat: Moved to support multiple strings and tags based on regex hits
    Settings are now completely different. Removed a bunch and added lists for urls and strings.

--- src/main.tsx
+++ src/main.tsx
@@ -32,68 +32,58 @@ async function updateJiraActivity() {
           logseq.Editor.updateBlock(currentBlock.uuid, "Appending Jira Activities from Stream Url...")
           const afterDate = moment(`${currentPage.journalDay}`, "YYYYMMDD")
           const beforeDate = moment(`${currentPage.journalDay}`, "YYYYMMDD").subtract(1, 'millisecond').add(1, 'day')
-          let activityStreamUrl = `${logseq.settings?.host}activity`
-          activityStreamUrl += "?maxResults=200"
-          const reaccount_id = new RegExp("&streams=account-id.*", "g")
-          if (reaccount_id.test(logseq.settings?.account_id)) {
-            activityStreamUrl += logseq.settings?.account_id
-          }
-          const rekey = new RegExp("&streams=key.*", "g")
-          if (rekey.test(logseq.settings?.key)) {
-            activityStreamUrl += logseq.settings?.key
-          }
-          const reactivity = new RegExp("&issues=activity.*", "g")
-          if (reactivity.test(logseq.settings?.activity)) {
-            activityStreamUrl += logseq.settings?.activity
-          }
-          const reissue_type = new RegExp("&issues=issue_type.*", "g")
-          if (reissue_type.test(logseq.settings?.issue_type)) {
-            activityStreamUrl += logseq.settings?.issue_type
-          }
-          const reproduct_category = new RegExp("&issues=project_category.*", "g")
-          if (reproduct_category.test(logseq.settings?.project_category)) {
-            activityStreamUrl += logseq.settings?.project_category
-          }
-          activityStreamUrl += `&streams=update-date+BETWEEN+${afterDate.valueOf()}+${beforeDate.valueOf()}`
-          activityStreamUrl += "&os_authType=basic"
-          activityStreamUrl += `&title=Journal_Feed_${moment.now().valueOf()}`
-          console.info(activityStreamUrl)
-          const headers = new Headers()
-          headers.set('Authorization', 'Basic ' + encode(logseq.settings?.username + ":" + logseq.settings?.password));
-          const activitystreamxml = await fetch(activityStreamUrl, {
-            method: 'GET',
-            headers: headers
-          }).then(function(response) {
-            return response.text();
-          })
-          const xmlObj = xmlParser.parse(activitystreamxml)
-          if (xmlObj.feed.entry !== undefined){
-            for await (const thisEntry of xmlObj.feed.entry) {
-              const title:string = thisEntry.title
-              const reAuthor = new RegExp(`<a.*class="activity-item-user activity-item-author">${thisEntry.author.name}</a>`, "gm")
-              const reAssigneeOpen = new RegExp(`'<a.*class="activity-item-user">`, "gm")
-              const reAssigneeClose = new RegExp(`</a>'`, "gm")
-              const reSpaces = new RegExp("\\s\\s+", "gm")
-              const reApostrophe = new RegExp('&#39;', "gm")
-              const rePriority = new RegExp('\'#(.*?)\'', "gm")
-              const value = title
-                .replace(reAuthor,`[[${thisEntry.author.name}]]`)
-                .replace(reAssigneeOpen, '[[')
-                .replace(reAssigneeClose, ']]')
-                .replace(reSpaces, ' ')
-                .replace(reApostrophe, '\'')
-                .replace(rePriority, '$1')
-              const newblock = await logseq.Editor.insertBlock(
-                currentBlock.uuid,
-                value
-              )
-              if (newblock !== null && thisEntry.content !== undefined) {
-                const newblockchild = await logseq.Editor.insertBlock(
-                  newblock.uuid,
-                  thisEntry.content
+          for await (let activityStreamUrl of logseq.settings?.activityStreams.split('\n')) {
+            activityStreamUrl += `&streams=update-date+BETWEEN+${afterDate.valueOf()}+${beforeDate.valueOf()}`
+            console.info(activityStreamUrl)
+            const headers = new Headers()
+            headers.set('Authorization', 'Basic ' + encode(logseq.settings?.username + ":" + logseq.settings?.password));
+            const activitystreamxml = await fetch(activityStreamUrl, {
+              method: 'GET',
+              headers: headers
+            }).then(function (response) {
+              return response.text();
+            })
+            const xmlObj = xmlParser.parse(activitystreamxml)
+            if (xmlObj.feed.entry !== undefined) {
+              for await (const thisEntry of xmlObj.feed.entry) {
+                const title: string = thisEntry.title
+                const reSpaces = new RegExp("\\s\\s+", "gm")
+                const reApostrophe = new RegExp('&#39;', "gm")
+                const rePriority = new RegExp('\'#(.*?)\'', "gm")
+                let value = title
+                  .replace(reSpaces, ' ')
+                  .replace(reApostrophe, '\'')
+                  .replace(rePriority, '$1')
+                  .replace("'<", "<")
+                  .replace(">'", ">")
+                for (const match_and_tag of logseq.settings?.match_and_tag.split('\n')) {
+                  const match = match_and_tag.split('=')[0]
+                  const tag = match_and_tag.split('=')[1]
+                  const matchReg = new RegExp(match.split('/')[1], match.split('/')[2])
+                  if (value.match(matchReg)) {
+                    value += ` #[[${tag}]]`
+                  }
+                }
+                const newblock = await logseq.Editor.insertBlock(
+                  currentBlock.uuid,
+                  value
                 )
-                if (newblockchild !== null){
-                  await logseq.Editor.setBlockCollapsed(newblock.uuid, true)
+                if (newblock !== null && thisEntry.content !== undefined && thisEntry.content.length > 0) {
+                  let value = thisEntry.content
+                    .replace(reSpaces, ' ')
+                    .replace(reApostrophe, '\'')
+                  for (const match_and_tag of logseq.settings?.match_and_tag.split('\n')) {
+                    const match = match_and_tag.split('=')[0]
+                    const tag = match_and_tag.split('=')[1]
+                    const matchReg = new RegExp(match.split('/')[1], match.split('/')[2])
+                    if (value.match(matchReg)) {
+                      value += ` #[[${tag}]]`
+                    }
+                  }
+                  const newblockchild = await logseq.Editor.insertBlock(
+                    newblock.uuid,
+                    value
+                  )
                 }
               }
             }
